16891d65c68fbcbc1a859ee16935af494ec7c8a5
luis <luisbona37@gmail.com>
2019-07-01 10:48:13 +0200

    probando eslint

--- myapp/routes/api.js
+++ myapp/routes/api.js
@@ -1,54 +1,56 @@
-var express = require('express');
-var router = express.Router();
-var jwt = require('jsonwebtoken');
-let authController = require('../controllers/authController');
-let seriesController = require('../controllers/seriesController');
-//te crea el token
+const express = require('express');
+
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const authController = require('../controllers/authController');
+const seriesController = require('../controllers/seriesController');
+// te crea el token
 router.post('/auth', authController.find);
-//lita el usuario id
+// lita el usuario id
 router.get('/users/:id', authController.listOne);
-//listar usuarios 
+// listar usuarios
 router.get('/users', (req, res) => {
-    // const token = localStorage.getItem('token');
-    // token.authorization;
-    try {
-        const token = req.headers.authorization.replace('Bearer ', '');
-        // console.log(token);
-        let very5 = jwt.verify(token, "mysecret");
-        //también podemos hacer un if con un throw para que nos devuelva un error que nosotros le especifiquemos
-        (very5.isAdmin) ? authController.listAdmin(req, res) : authController.list(req, res);
-    } catch (error) {
-        res.status(400).send('El token no es válido o no hay token');
-    }
+  // const token = localStorage.getItem('token');
+  // token.authorization;
+  try {
+    const token = req.headers.authorization.replace('Bearer ', '');
+    // console.log(token);
+    const very5 = jwt.verify(token, 'mysecret');
+    // también podemos hacer un if con un throw para que nos devuelva un error
+    // que nosotros le especifiquemos
+    // eslint-disable-next-line no-unused-expressions
+    (very5.isAdmin) ? authController.listAdmin(req, res) : authController.list(req, res);
+  } catch (error) {
+    res.status(400).send('El token no es válido o no hay token');
+  }
 });
-//añadir un usuario
+// añadir un usuario
 router.post('/users', authController.add);
-//editar un usuario
+// editar un usuario
 router.put('/users/:id', authController.edit);
-//borrar un usuario
+// borrar un usuario
 router.delete('/users/:id', authController.delete);
 
 
- 
-//lista las series
-router.get('/series', seriesController.listAdmin);//(req, res) => {
-    // try {
-    //     const token = req.headers.authorization.replace('Bearer ', '');
-    //     let very5 = jwt.verify(token, "mysecret");
-    //     seriesController.listAdmin(req, res);
-    //     // (very5.isAdmin) ? seriesController.listAdmin(req, res) : seriesController.list(req, res);
-    // } catch (error) {
-    //     res.status(400).send('El token no es válido o no hay token');
-    // }
+// lista las series
+router.get('/series', seriesController.listAdmin);// (req, res) => {
+// try {
+//     const token = req.headers.authorization.replace('Bearer ', '');
+//     let very5 = jwt.verify(token, "mysecret");
+//     seriesController.listAdmin(req, res);
+//     // (very5.isAdmin) ? seriesController.listAdmin(req, res) : seriesController.list(req, res);
+// } catch (error) {
+//     res.status(400).send('El token no es válido o no hay token');
+// }
 // });
-//lita una serie id
+// lita una serie id
 router.get('/series/:idS/:idU', seriesController.listOne);
-//listar las series del User IdU
+// listar las series del User IdU
 router.get('/series/:idU', seriesController.listSeriesUser);
-//añadir una serie
+// añadir una serie
 router.post('/series', seriesController.add);
-//editar una serie idU es el id Usuario que creo la pelicula
+// editar una serie idU es el id Usuario que creo la pelicula
 router.put('/series/edit/:idS/:idU', seriesController.edit);
-//borrar una serie solo puede hacerlo el Admin
+// borrar una serie solo puede hacerlo el Admin
 router.delete('/series/:id', seriesController.delete);
 module.exports = router;
